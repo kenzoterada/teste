3eaf5ab52f76d656556c80de06d78f4a8b809733
Vitor Kenzo Terada <vitorkenzoterada@gmail.com>
2023-04-29 21:04:31 -0300

    primeiro envio

---
+++ teste.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="gabriela testuda", page_icon=":person_bald:", layout="wide")
+
+st.subheader("gabriela é muito testuda")
+st.title("Gabriela Saemi Hyun tem uma caixa d'água")
