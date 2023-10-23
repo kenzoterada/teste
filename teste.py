528fb6135cd6aeeebdeb318c76b1b84983ed738d
Vitor Kenzo Terada <104955407+kenzoterada@users.noreply.github.com>
2023-10-23 13:22:36 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,1 @@
-import streamlit as st
 
-st.set_page_config(page_title="gabriela testuda", page_icon=":person_bald:", layout="wide")
-
-st.subheader("test upload site")
-st.title("teste novo site")
